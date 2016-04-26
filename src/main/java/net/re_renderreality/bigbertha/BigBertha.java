105ce6201fb10f0772c6b0daba7095e624a5cdee
Alex Hoffmann <alexhoff@umich.edu>
2016-04-26 17:35:40 -0400

    Initial Commit

--- src/main/java/net/re_renderreality/bigbertha/BigBertha.java
+++ src/main/java/net/re_renderreality/bigbertha/BigBertha.java
@@ -1,15 +1,15 @@
-package com.example.examplemod;
+package net.re_renderreality.bigbertha;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = BigBertha.MODID, version = BigBertha.VERSION)
+public class BigBertha
 {
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "bigbertha";
+    public static final String VERSION = "0.0.1";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
